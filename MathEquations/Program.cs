86f8e899a49aceaf2cae21db9c84e79300ed9fbd
MrGreenGaming <37852707+MrGreenGaming14@users.noreply.github.com>
2023-04-22 18:29:13 -0500

    Ronald's Script Pt 2

--- MathEquations/Program.cs
+++ MathEquations/Program.cs
@@ -6,14 +6,18 @@ namespace MathEquations
 {
     class MainMath
     {
-        private static int Num1;
-        private static int Num2;
-        private static int C = 0;
+        private static double Num1;
+        private static double Num2;
+        private static double C = 0;
+        private static String plus = "+";
+        private static String minus = "-";
+        private static String divide = "/";
+        private static String multiply = "*";
 
 
         public static void Main(String[] args) 
         {
-            randEquation();
+           randEquation();
         }
 
 
@@ -37,43 +41,58 @@ namespace MathEquations
             switch(index)
             {
                 case 0:
-                    Console.WriteLine(Num1 + " + " + Num2 + " = " + addMethod(Num1, Num2));
+                    Console.WriteLine(Num1 + plus + Num2 + " = " + addMethod(Num1, Num2));
                     break;
                 case 1:
-                    Console.WriteLine(Num1 + " - " + Num2 + " = " + subMethod(Num1, Num2));
+                    Console.WriteLine(Num1 + minus + Num2 + " = " + subMethod(Num1, Num2));
                     break;
                 case 2:
-                    Console.WriteLine(Num1 + " * " + Num2 + " = " + multMethod(Num1, Num2));
+                    Console.WriteLine(Num1 + multiply + Num2 + " = " + multMethod(Num1, Num2));
                     break;
                 case 3:
-                    Console.WriteLine(Num1 + " / " + Num2 + " = " + divMethod(Num1, Num2));
+                    Console.WriteLine(Num1 + divide + Num2 + " = " + divMethod(Num1, Num2));
                     break;
             }
             Console.WriteLine("\n" + C);
             
         }
 
-        public static int addMethod(int a, int b)
+        public static double addMethod(double a, double b)
         {
             C = a + b;
-            return a + b;
+            return C;
         }
 
-        public static int subMethod(int a, int b)
+        public static double subMethod(double a, double b)
         {
-            C = Math.Abs(a-b);
-            return Math.Abs(a-b);
+            if(b > a)
+            {
+                double d = b;
+                b = a;
+                a = d;
+            }
+
+            C = a-b;
+            return C;
         }
 
-        public static int multMethod(int a, int b)
+        public static double multMethod(double a, double b)
         {
             C = a * b;
-            return a * b;
+            return C;
         }
 
-        public static int divMethod(int a, int b)
+        public static double divMethod(double a, double b)
         {
-            return 0;
+            if(b > a)
+            {
+                double d = b;
+                b = a;
+                a = d;
+            }
+
+            C = Math.Round(a/b,2);
+            return C;
         }
 
     }
