8602b454517b30cd6998dee9c6df1f472da2176f
Parker Posey <mparkerposey@outlook.com>
2023-04-23 02:26:32 -0500

    added equation checking from connected blocks

--- Assets/Scripts/Box.cs
+++ Assets/Scripts/Box.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using TMPro;
 
 public class Box : MonoBehaviour
 {
@@ -10,17 +12,67 @@ public class Box : MonoBehaviour
     
     //flags
     public bool canBeGrabbed;
+    public bool rightSnapping;
+    public bool leftSnapping;
+    public bool playerTouching;
+    public bool snapping;
 
     //references
-    private PlayerController playerController;
+    public PlayerController playerController;
     private Rigidbody2D blockRb;
-    private Rigidbody2D playerRb;
+    public Rigidbody2D playerRb;
+    private float blockWidth;
+    private SpriteRenderer blockRend;
+    private GameObject leftBlock;
+    private GameObject rightBlock;
+
+    //math stuff
+    public GameManager gameManager;
+    public string value = "";
+    public TextMeshProUGUI blockText;
+    public Vector3 position;
 
     void Start()
     {
         playerController = GameObject.Find("Player").GetComponent<PlayerController>();
         playerRb = GameObject.Find("Player").GetComponent<Rigidbody2D>();
         blockRb = GetComponent<Rigidbody2D>();
+        blockRend = GetComponent<SpriteRenderer>();
+        blockWidth = blockRend.bounds.size.x;
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+
+        switch(this.gameObject.name)
+        {
+            case "Block":
+                value = gameManager.a.ToString();
+                position = gameManager.spawnPositions[gameManager.randomArray[0]];
+                break;
+
+            case "Block (1)":
+                value = gameManager.b.ToString();
+                position = gameManager.spawnPositions[gameManager.randomArray[1]];
+                break;
+
+            case "Block (2)":
+                value = gameManager.c.ToString();
+                position = gameManager.spawnPositions[gameManager.randomArray[2]];
+                break;
+
+            case "Block (3)":
+                value = gameManager.d;
+                position = gameManager.spawnPositions[gameManager.randomArray[3]];
+                break;
+
+            case "Block (4)":
+                value = gameManager.e;
+                position = gameManager.spawnPositions[gameManager.randomArray[4]];
+                break;
+        }
+
+        blockText.text = value;
+        this.gameObject.transform.position = position;
+
+        
     }
 
     // Update is called once per frame
@@ -50,18 +102,38 @@ public class Box : MonoBehaviour
         //player grabbing block
         if (playerController.isGrabbing && playerController.currentBlock == this.gameObject)
         {
-            playerController.speed = 3;
             blockRb.drag = 0;
             blockRb.angularDrag = 0.05f;
             blockRb.velocity = playerRb.velocity;
         }
         else
         {
-            playerController.speed = 5;
             blockRb.drag = 20;
             blockRb.angularDrag = 1;
             blockRb.velocity = new Vector3(0, 0, 0);
+        }
 
+        if(!playerController.isGrabbing && !playerTouching)
+        {            
+            if (rightSnapping)
+            {
+                transform.position = new Vector3(rightBlock.transform.position.x - blockWidth/1.8f, rightBlock.transform.position.y, 0);
+            }
+            else if (leftSnapping)
+            {
+                transform.position = new Vector3(leftBlock.transform.position.x + blockWidth/1.8f, leftBlock.transform.position.y, 0);
+            }
+
+
+        }
+
+        if (leftSnapping || rightSnapping)
+        {
+            snapping = true;
+        }
+        else
+        {
+            snapping = false;
         }
     }
 
@@ -72,7 +144,21 @@ public class Box : MonoBehaviour
             canBeGrabbed = true;
             playerController.canGrab = true;
             playerController.currentBlock = this.gameObject;
-            Debug.Log("can grab");
+        }
+
+        if (other.CompareTag("BlockDetector"))
+        {
+            if (other.gameObject.transform.position.x < this.gameObject.transform.position.x)
+            {
+                leftSnapping= true;
+                leftBlock = other.gameObject;
+            }
+
+            if (other.gameObject.transform.position.x > this.gameObject.transform.position.x)
+            {
+                rightSnapping = true;
+                rightBlock = other.gameObject;
+            }
         }
     }
 
@@ -83,8 +169,37 @@ public class Box : MonoBehaviour
             canBeGrabbed = false;
             playerController.canGrab = false;
             playerController.currentBlock = null;
-            Debug.Log("can not grab");
+        }
+
+        if (other.CompareTag("BlockDetector"))
+        {
+            if (other.gameObject.transform.position.x < this.gameObject.transform.position.x)
+            {
+                leftSnapping = false;
+                leftBlock = null;
+            }
+
+            if (other.gameObject.transform.position.x > this.gameObject.transform.position.x)
+            {
+                rightSnapping = false;
+                rightBlock = null;
+            }
         }
     }
-    
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Player"))
+        {
+            playerTouching = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Player"))
+        {
+            playerTouching = false;
+        }
+    }
 }
